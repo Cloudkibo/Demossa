f5d454207fbe8a7c9c67aba472bf5024d44e5dc7
Glitch (hello-express) <none>
2018-10-31 03:19:49 +0000

    👳👋 Checkpoint ./server.js:2098456/41 ./views/estimatedBenefits.html:2098456/2597

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/updateContactInformation.html');
+  response.sendFile(__dirname + '/views/estimatedBenefits.html');
 });
 
 const createButtons = (displayUrl) => {
