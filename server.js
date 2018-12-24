5996e431812aadb0721d047d5a56fc2f31b74e04
Glitch (hello-express) <none>
2018-12-24 08:58:06 +0000

    🌞🏏 Checkpoint ./server.js:2098456/196

--- server.js
+++ server.js
@@ -30,6 +30,12 @@ app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/earningsRecord.html');
