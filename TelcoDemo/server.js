c441e3f3777ad6cfdf8c8078356fc6da9e30c843
Sojharo <sojharo@live.com>
2019-04-30 10:20:10 +0500

    ssl mistake corrected

--- TelcoDemo/server.js
+++ TelcoDemo/server.js
@@ -10,7 +10,11 @@ const ailayer = require('./ai-layer.js')
 const util = require('./utility.js')
 const platforms = require('./platforms.js')
 
-var app = express();
+var httpsApp = express();
+var httpApp = express()
+
+const app = (process.env.NODE_ENV === 'production') ? httpsApp : httpApp
+
 
 // Setup template engine - add pug
 app.set('view engine', 'pug');
@@ -190,7 +194,43 @@ app.get('/*', (request, response) => {
   response.send('This page is not yet implemented in our demo.')
 })
 
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+
+let options = {
+    ca: '',
+    key: '',
+    cert: ''
+}
+
+if (process.env.NODE_ENV === 'production') {
+  try {
+    options = {
+      ca: fs.readFileSync('/root/certs/ca_bundle.crt'),
+      key: fs.readFileSync('/root/certs/private.key'),
+      cert: fs.readFileSync('/root/certs/certificate.crt')
+    }
+  } catch (e) {
+  }
+}
+
+const server = http.createServer(httpApp)
+const httpsServer = https.createServer(options, httpsApp)
+
+if (process.env.NODE_ENV === 'production') {
+  httpApp.get('*', (req, res) => {
+    res.redirect(`${process.env.DOMAIN}${req.url}`)
+  })
+}
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+server.listen(process.env.PORT, process.env.IP, () => {
+  console.log(`DEMOSSA server STARTED on ${
+    process.env.PORT} in ${process.env.NODE_ENV} mode on domain ${process.env.DOMAIN}`)
+})
+
+httpsServer.listen(process.env.SECURE_PORT, () => {
+console.log(`DEMOSSA server STARTED on ${
+  process.env.SECURE_PORT} in ${process.env.NODE_ENV} mode on domain ${process.env.DOMAIN}`)
+})
