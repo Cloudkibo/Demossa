10578a7d1441d2979f44c81d2de6ef48eca3498d
Cloudkibo-Faizan <faizanrasheed4@gmail.com>
2019-05-02 17:32:06 +0500

    development setup

---
+++ TelcoDemo/config/environment/staging.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sojharo on 14/11/2017.
+ */
+ 'use strict'
+
+ // Development specific configuration
+ // ==================================
+ module.exports = {
+   // MongoDB connection options
+   mongo: {
+     uri: 'mongodb://localhost/telcoDemo-staging'
+   },
+   seedDB: false
+ }
