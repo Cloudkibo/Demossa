d00eeed77473ad98cc8e341d25309fcf8f297411
ImranBinShoukat <imran.shoukat1995@gmail.com>
2021-01-20 14:42:58 +0500

    serve static files

--- KiboAutomation/routes.js
+++ KiboAutomation/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 module.exports = function (app) {
   // Parse data from application/x-www-form-urlencoded
@@ -7,8 +8,9 @@ module.exports = function (app) {
   app.use(bodyParser.json())
 
   // http://expressjs.com/en/starter/static-files.html
-  app.use(express.static('public'))
+  app.use(express.static(path.join(__dirname, 'public')))
   app.use('/', require('./api/chatbot'))
+
   app.get('/*', (request, response) => {
     response.send('This page is not yet implemented.')
   })
